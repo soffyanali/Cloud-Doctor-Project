84a1dbedf6bef406a160f0feb31af92a705470a1
Soffyan Ali <soffyanali@gmail.com>
2017-07-24 17:42:33 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   resources :pt_records
+  root 'pt_records#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
